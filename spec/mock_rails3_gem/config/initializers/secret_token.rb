cdf2667b5879a64352c10ef610239c565402b917
Karl Varga <kjvarga@gmail.com>
2010-04-19 11:52:30 -0700

    Clean Rails 3 beta 3 install.

---
+++ spec/mock_rails3_gem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'c71ae7783b09942af1f21d50aae4552e7ebb316ace7ec011eb0fb4773473acacb579f540557a8c27ee71ee9e05c75298320382a6f7e77ba0bfb8cf77bacc1ab2'
